80552a64395e772a2c4df7da6bf94303c065c614
Student User <klaus_bonn@posteo.de>
2026-09-15 09:03:19 -0400

    unit-testing

--- school-library/src/test/java/com/redhat/training/LibraryWithMockedInventoryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryWithMockedInventoryTest.java
@@ -36,14 +36,14 @@ public class LibraryWithMockedInventoryTest {
 
     @Test
     public void checkingOutDoesNotWithdrawFromInventoryWhenBookIsUnavailable()
-    throws BookNotAvailableException {
-        // Given
-        when(inventory.isBookAvailable("book1")).thenReturn(false);
-        // When
-        try {
-            library.checkOut("student1", "book1");
-        } catch(BookNotAvailableException e) {}
-        // Then
-        verify(inventory, times(0)).withdraw("book1");
+        throws BookNotAvailableException {
+            // Given
+            when(inventory.isBookAvailable("book1")).thenReturn(false);
+            // When
+            try {
+                library.checkOut("student1", "book1");
+            } catch(BookNotAvailableException e) {}
+            // Then
+            verify(inventory, times(0)).withdraw("book1");
     }
 }
